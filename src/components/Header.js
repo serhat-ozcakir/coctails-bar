e44ef5dcfc91223040084e7173b29da1dd74c99d
serhat-ozcakir <serhatcakir87@gmail.com>
2022-02-27 22:00:16 +0300

    update comit

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav class="navbar  navbar-dark bg-primary ">
-      <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Cocktail Bar</span>
+    <nav className="navbar  navbar-dark bg-primary ">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">Cocktail Bar</span>
       </div>
     </nav>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,28 @@
 import React, { useState, useContext } from "react";
 import { ContextBarContext } from "../context/ContextBar";
-import logo from "../image/3747371.jpg"
+import logo from "../image/3747371.jpg";
+import newLogo from "../image/iced-cocktails-drinking-glass.jpg";
 
 const Home = () => {
   const { cocktails, ingredients, glasses, categories, alcoholic_types } =
     useContext(ContextBarContext);
 
   const [drinks, setDrinks] = useState([
-    { category: "", drinkType: "", glassType: ""}
+    { category: "", drinkType: "", glassType: "" },
   ]);
 
-  // console.log("cocktails :>> ", cocktails.drinks);
-  // console.log(ingredients);
-  // const ing = []
-  // console.log('ing', ing)
-
-  // console.log("drinks :>> ", drinks);
   const [filteredDrink, setFilteredDrink] = useState([]);
-  
+  const [main, setMain] = useState(true);
+  const [enter, setEnter] = useState(true);
+  // console.log(ingredients);
+
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setDrinks({ ...drinks, [name]: value });
-    
+
     // setIng({...ing, [name]: value })
   };
-  
-
-
-
 
   const filterDrink = cocktails?.drinks?.filter((item) => {
     if (
@@ -36,38 +31,32 @@ const Home = () => {
       item?.strGlass === drinks?.glassType
       //  &&
       // item?.strIngredient1 === drinks?.ingType
-      ) {
-        return true;
-      }
-    });
-    const [main, setMain] = useState(true)
-    // console.log('main :>> ', main);
+    ) {
+      return true;
+    }
+  });
+
   // console.log("filterDrink :>> ", filterDrink);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setFilteredDrink(filterDrink);
-    const { value } = e.target;
-    // setDrinks({ ...drinks});
-    console.log(filterDrink);
-    
-    // console.log(filteredDrink);
-    if (filterDrink.length == 0){
-      setMain(false)
-    } 
-    
+    setEnter(false);
+    if (filterDrink.length == 0) {
+      setMain(false);
+    }
   };
 
   return (
     <div className="row">
       <div className="col-xl-4 col-sm-12">
-        <form className="container w-50 mt-5 border" onSubmit={handleSubmit} >
-          <div className="">
-            <h1>Order a drink</h1>
+        <form className="container w-50 mt-5 border" onSubmit={handleSubmit}>
+          <div >
+            <h1 className="text-primary fs-2">Order a drink</h1>
           </div>
           <div className=" mb-3 ">
-            <label for="name" className="form-label">
-              Cocktail bar
+            <label htmlFor="name" className="form-label">
+              Cocktail name
             </label>
             <input
               type="text"
@@ -81,12 +70,12 @@ const Home = () => {
             />
           </div>
           <div className=" mb-3 ">
-            <label for="full name" className="form-label">
-              Full Name
+            <label htmlFor="full name" className="form-label">
+              Full name
             </label>
             <input
               type="text"
-              class="form-control"
+              className="form-control"
               id="title"
               name="full_name"
               required
@@ -96,7 +85,7 @@ const Home = () => {
             />
           </div>
           <div className=" mb-3 ">
-            <label for="url" class="form-label">
+            <label htmlFor="url" className="form-label">
               Phone number
             </label>
             <input
@@ -111,7 +100,7 @@ const Home = () => {
             />
           </div>
           <div className=" mb-3 ">
-            <label for="url" class="form-label">
+            <label htmlFor="url" className="form-label">
               Email adress
             </label>
             <input
@@ -125,9 +114,9 @@ const Home = () => {
               onChange={handleChange}
             />
           </div>
-          <div class="input-group mb-3">
+          <div className="input-group mb-3">
             <select
-              class="form-select"
+              className="form-select"
               id="inputGroupSelect03"
               aria-label="Example select with button addon"
               onChange={handleChange}
@@ -138,14 +127,16 @@ const Home = () => {
                 Category
               </option>
 
-              {categories?.drinks?.map((item,index) => (
-                <option key={index} value={item?.strCategory}>{item?.strCategory}</option>
+              {categories?.drinks?.map((item, index) => (
+                <option key={index} value={item?.strCategory}>
+                  {item?.strCategory}
+                </option>
               ))}
             </select>
           </div>
-          <div class="input-group mb-3">
+          <div className="input-group mb-3">
             <select
-              class="form-select"
+              className="form-select"
               id="inputGroupSelect03"
               aria-label="Example select with button addon"
               onChange={handleChange}
@@ -155,14 +146,16 @@ const Home = () => {
               <option value="" selected disabled>
                 Type of drink
               </option>
-              {alcoholic_types?.drinks?.map((item,index) => (
-                <option key={index} value={item?.strAlcoholic}>{item?.strAlcoholic}</option>
+              {alcoholic_types?.drinks?.map((item, index) => (
+                <option key={index} value={item?.strAlcoholic}>
+                  {item?.strAlcoholic}
+                </option>
               ))}
             </select>
           </div>
-          <div class="input-group mb-3">
+          <div className="input-group mb-3">
             <select
-              class="form-select"
+              className="form-select"
               id="inputGroupSelect03"
               aria-label="Example select with button addon"
               onChange={handleChange}
@@ -172,12 +165,14 @@ const Home = () => {
               <option value="" selected disabled>
                 Type of glass
               </option>
-              {glasses?.drinks?.map((item,index) => (
-                <option key={index} value={item?.strGlass}>{item?.strGlass}</option>
+              {glasses?.drinks?.map((item, index) => (
+                <option key={index} value={item?.strGlass}>
+                  {item?.strGlass}
+                </option>
               ))}
             </select>
           </div>
-{/* 
+          {/* 
           <div class="input-group mb-3">
             <select
               class="form-select"
@@ -199,53 +194,71 @@ const Home = () => {
             )} 
             </div>
             </div> */}
-          <button
-            type="submit"
-            // onClick={handleSubmit}
-            // className="btn btn-primary"
-            className="btn btn-primary"
-          >
+          <button type="submit" className="btn btn-primary">
             Search
           </button>
         </form>
       </div>
-{/* {filteredDrink.length == 0 ? <div>araba</div> : null } */}
-      { 
-      main  ?
-      filteredDrink?.map((item) => (
-        // <div className="container">
+
+      {enter ? (
+        <div className="col-xl-4 col-sm-12">
+          <div
+            className="card mt-5 p-3 mx-3"
+            style={{ minHeight: 440, minWidth: 750 }}
+          >
+            <h5 className="card-title mt-1 text text-center text-primary fs-3 fst-italic">
+              Welcome to my cocktails bar
+            </h5>
+            <img
+              src={newLogo}
+              class="card-img-top"
+              alt="..."
+              style={{ height: 250 }}
+              className="mt-2"
+            />
+            <p className="mt-2 fst-italic fw-bold">
+              Delicious drinks await you. You can choose very special drinks for
+              yourself at the Cocktail Bar. All you have to do is set your
+              wishes and then enjoy yourself...
+            </p>
+          </div>
+        </div>
+      ) : main ? (
+        filteredDrink?.map((item) => (
+          // <div className="container">
           <div className="col-xl-4 col-sm-12">
-            <div class="card mt-5 p-3 mx-3" style={{ minHeight: 440 }}>
+            <div className="card mt-5 p-3 mx-3" style={{ minHeight: 440 }}>
               <img
                 src={item.strDrinkThumb}
-                class="card-img-top"
+                className="card-img-top"
                 alt="..."
                 style={{ height: 250 }}
               />
-              <h5 class="card-title mt-1">{item.strDrink}</h5>
-              <p class="card-text">{item.strInstructions}</p>
-              <div class="card-body"></div>
+              <h5 className="card-title mt-1">{item.strDrink}</h5>
+              <p className="card-text">{item.strInstructions}</p>
+              <div className="card-body"></div>
             </div>
           </div>
-        // </div>
-      ))
-       : 
-      
-       <div className="col-xl-4 col-sm-12">
-       <div class="card mt-5 p-3 mx-3" style={{ minHeight: 440 }}>
-         <h5 class="card-title mt-1 text">Sorry, no beverage found matching your search criteria.</h5>
-         <img
-           src={logo}
-           class="card-img-top"
-           alt="..."
-           style={{ height: 250 }}
-         />
-         
-         
-       </div>
-     </div>
-
-      }
+          // </div>
+        ))
+      ) : (
+        <div className="col-xl-4 col-sm-12">
+          <div
+            className="card mt-5 p-3 mx-3"
+            style={{ minHeight: 440, minWidth: 600 }}
+          >
+            <h5 className="card-title mt-1 text-center text-danger">
+              Sorry, no beverage found matching your search criteria.
+            </h5>
+            <img
+              src={logo}
+              className="card-img-top"
+              alt="..."
+              style={{ height: 250 }}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
